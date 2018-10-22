c4f85dbaf1ee6f22278a98ea0261fa694f8ab4c0
rd568 <bobbyday3620@gmail.com>
2018-10-22 11:21:14 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    //The setup
+        String names[] = {"Bobby", "Abby", "Megan", "Fabi", "Chris", "Chief", "Bryce", "Ian", "Alex", "Jawn", "Ian", "Sam", "Haley", "Briteny", "Heather", "Jaden",
+        "Leah", "Wyatt", "Kevin", "Rob", "Ellie", "Emily", "Serena", "Steven", "Rocky", "Ella", "Jarret", "Brooke", "Sixnine", "Kayne", "Mac Miller", "Bennet",
+        "Prem", "Mat", "Kyle", "Ben", "XXXTentacion(RIP)", "Drake", "Drew", "Andrew", "Kylie", ""};
+
+
+
+
+        Scanner kbInput = new Scanner(System.in);
+
+    }
+}
---
+++ src/com/company/theNumber.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by rd568 on 10/22/18.
+ */
+public class theNumber {
+}
